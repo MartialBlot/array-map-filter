f98b9522c6f419dbb62f4d6fd343477082bfef8a
BLOT Martial <martial.blot@outlook.fr>
2019-03-22 11:22:12 +0100

    start ex

--- .history/filter/ex1_20190322111831.js
+++ .history/filter/ex1_20190322111831.js
@@ -12,7 +12,7 @@ Sortie attendue:
  */
 
 function getPositiveNumbers(numbers) {
-  return numbers = numbers.filter (numbers => numbers >= 0);
+  return numbers.filter = numbers => numbers >= 0;
 }
 // Si besoin, tu peux toujours visualiser le résultat avec console.log,
 // mais alors, il faut alors lancer l'exercice avec node filter/ex1
